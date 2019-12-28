c5dd0f9a56558820db2a936a4dceba9d213bf93e
ashisjena <ashisjena.talk2u@gmail.com>
2019-12-28 17:46:43 +0530

    19/12/28_OfficeLaptop

---
+++ src/main/kotlin/com/ashis/lamdacollectiongenerics/Lamdas.kt
@@ -0,0 +1,43 @@
+package com.ashis.lamdacollectiongenerics
+
+import java.util.*
+
+fun main() {
+    run { println("I'm in a lambda!") }
+
+    val employees = arrayListOf(Employee("John", "Smith", 2012),
+            Employee("Jane", "Wilson", 2020),
+            Employee("Mike", "Monegan", 2010),
+            Employee("Marry", "Johnson", 2000))
+
+    println(employees.minBy { e -> e.startYear })
+    println(employees.minBy { it.startYear })
+    println(employees.minBy(Employee::startYear))
+
+    var num = 10
+    run {
+        num += 15
+        println(num)
+    }
+
+    println(countTo50())
+    println(countTo60())
+    println(countTo55())
+}
+
+fun countTo50(): String {
+    val numbers = StringJoiner(", ")
+    (1..50).forEach { numbers.add(it.toString()) }
+    return numbers.toString()
+}
+
+fun countTo60(): String = with(StringJoiner(", ")) {
+    (1..60).forEach { this.add(it.toString()) }
+    this.toString()
+}
+
+fun countTo55(): String = StringJoiner(", ").apply {
+    (1..55).forEach { this.add(it.toString()) }
+}.toString()
+
+data class Employee(val firstName: String, val lastName: String, val startYear: Int)
